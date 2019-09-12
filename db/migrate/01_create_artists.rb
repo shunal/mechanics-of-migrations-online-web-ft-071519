10206522585040721d151588cfde9192da829e57
Steven Hunal <stevehunal@gmail.com>
2019-09-12 03:58:26 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -8,25 +8,3 @@ class CreateArtists < ActiveRecord::Migration[4.2]
     end
   end
 end
-
-class CreateCostumes < ActiveRecord::Migration[5.2]
-  create_table :costumes do |x|
-    x.string :name
-    x.integer :price
-    x.string :size
-    x.string :image_url
-  end 
-end 
-
-class CreateCostumeStores < ActiveRecord::Migration[5.2]
-  
-  create_table :costume_stores do |column|
-    column.string :name
-    column.string :location
-    column.integer :costume_inventory
-    column.integer :number_of_employees
-    column.string :in_business?
-    column.integer :opening_time
-    column.integer :closing_time
-  end 
-end 
