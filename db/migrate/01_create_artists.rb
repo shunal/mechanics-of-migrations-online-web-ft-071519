09e45e267782dbfa3380eed65bed615782d7a994
Steven Hunal <stevehunal@gmail.com>
2019-09-12 03:50:27 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -8,3 +8,12 @@ class CreateArtists < ActiveRecord::Migration[4.2]
     end
   end
 end
+
+class CreateCostumes < ActiveRecord::Migration[5.2]
+  create_table :costumes do |x|
+    x.string :name
+    x.integer :price
+    x.string :size
+    x.string :image_url
+  end 
+end 
